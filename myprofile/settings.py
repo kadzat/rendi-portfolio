8a0b70d7c82c7a405203912704d7ffd1950dc556
kadzat <kadzat.leusmana14@gmail.com>
2021-11-18 19:31:05 +0700

    membuat aplikasi portfolio sederhana

--- myprofile/settings.py
+++ myprofile/settings.py
@@ -41,10 +41,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-
     'whitenoise.middleware.WhiteNoiseMiddleware',
-
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,8 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-]
+)
